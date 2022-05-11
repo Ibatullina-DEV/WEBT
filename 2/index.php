bc44b13b9fbfed9bddc258219040bb83a8f00bb2
Ibatullina-DEV <81437438+Ibatullina-DEV@users.noreply.github.com>
2022-05-12 02:18:32 +0500

    Загрузила лабу №2

---
+++ 2/index.php
@@ -0,0 +1,18 @@
+<html>
+<head> <title> Лаба 2 </title> </head>
+<div align="left">
+  <a  href="http://a0670919.xsph.ru/index.php">Назад </a>  
+</div>
+<body align="left">
+<h1>Лаба 2</h1><p>
+<h2>Ибатуллина А.И. ПИ-323</h2><p>
+<u1>
+        <li><a href="http://a0670919.xsph.ru/2/2-1.html">Задание - 1. </a></li>
+        <li><a href="http://a0670919.xsph.ru/2/2-2.html">Задание - 2. </a></li>
+        <li><a href="http://a0670919.xsph.ru/2/2-3.html">Задание - 3. </a></li>
+        <li><a href="http://a0670919.xsph.ru/2/2-4.html">Задание - 4. </a></li>
+        <li><a href="http://a0670919.xsph.ru/2/2-5.html">Задание - 5. </a></li>
+        <li><a href="http://a0670919.xsph.ru/2/2-6.html">Задание - 6. </a></li>
+        <li><a href="http://a0670919.xsph.ru/2/2-7.html">Задание - 7. </a></li>
+</u1>
+</html>
